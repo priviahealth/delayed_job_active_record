d311b55f1eb4567d18627b8424c9042b0260776f
Nathan Broadbent <nathan.f77@gmail.com>
2014-08-29 18:13:07 -0700

    Revert to more stable lookup without locking issues

--- lib/delayed/backend/active_record.rb
+++ lib/delayed/backend/active_record.rb
@@ -66,10 +66,9 @@ module Delayed
             reserved          = find_by_sql(["UPDATE #{quoted_table_name} SET locked_at = ?, locked_by = ? WHERE id IN (#{subquery_sql}) RETURNING *", now, worker.name])
             reserved[0]
           when 'MySQL', 'Mysql2'
-            # This works on MySQL and possibly some other DBs that support UPDATE...LIMIT. It uses separate queries to lock and return the job
-            count = ready_scope.limit(1).update_all(:locked_at => now, :locked_by => worker.name)
-            return nil if count == 0
-            where(:locked_at => now, :locked_by => worker.name, :failed_at => nil).first
+            # Revert to more stable lookup without locking issues
+            reserve_with_scope_using_default_sql(ready_scope, worker, now)
+
           when 'MSSQL', 'Teradata'
             # The MSSQL driver doesn't generate a limit clause when update_all is called directly
             subsubquery_sql = ready_scope.limit(1).to_sql
